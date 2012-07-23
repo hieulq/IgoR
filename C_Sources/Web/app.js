6e8c5d6b5629021d93f2cdde53b047230f4f4a14
hieulq <hieulq89@gmail.com>
2012-07-23 16:05:53 +0700

    Adding new logo & build for Android 	modified:   C_Sources/Web/app.js 	new file:   C_Sources/Web/archive/app.js/4f0999b96ae82e39711a7360a1230c4e050dd55b 	new file:   C_Sources/Web/archive/app.js/7387c4fabfa1b894fd0ccb20b1e6bc8a76accb29 	new file:   C_Sources/Web/archive/resources/css/app.css/f5d2844927f8d50dedac0f9749506ff484928c84 	modified:   C_Sources/Web/build/production/app.js 	modified:   C_Sources/Web/build/production/app.json 	modified:   C_Sources/Web/build/production/cache.manifest 	new file:   C_Sources/Web/build/production/data/notifications.json 	new file:   C_Sources/Web/build/production/data/profiles.json 	new file:   C_Sources/Web/build/production/data/tasks.json 	new file:   C_Sources/Web/build/production/data/users.json 	modified:   C_Sources/Web/build/production/deltas/app.js/24fdb1f5c8f721504a5707e2ee85839458e2fb8d.json 	new file:   C_Sources/Web/build/production/deltas/app.js/278865feb5414d7a2511a550f1d0867af75273ba.json 	new file:   C_Sources/Web/build/production/deltas/app.js/4f0999b96ae82e39711a7360a1230c4e050dd55b.json 	modified:   C_Sources/Web/build/production/deltas/app.js/d5d6212cf107ebe0d6c5a04450f2f7db02014838.json 	new file:   C_Sources/Web/build/production/deltas/resources/css/app.css/0f88538b227756c186ea83f65eabed19a6aefa2e.json 	modified:   C_Sources/Web/build/production/deltas/resources/css/app.css/9d651c617c449d3f4d22be89186b2cb784165276.json 	modified:   C_Sources/Web/build/production/index.html 	modified:   C_Sources/Web/build/production/resources/css/app.css 	new file:   C_Sources/Web/build/production/resources/images/Igor.png 	new file:   C_Sources/Web/build/production/resources/images/google-icon.png 	modified:   C_Sources/Web/resources/images/Igor.png

--- C_Sources/Web/app.js
+++ C_Sources/Web/app.js
@@ -28,7 +28,7 @@ Ext.application({
 
         // Initialize the main view
         Ext.Viewport.add({
-            xclass: 'Igor.view.Main'
+            xclass: 'Igor.view.authenticate.Authenticate'
         });
     },
 
