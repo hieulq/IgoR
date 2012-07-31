7d155f035c03e3c044a8b8d42229c1db69a3a1ce
phucnh <nguyenhongphuc.hut@gmail.com>
2012-07-31 12:07:41 +0700

    - add total_session to class mobile data template - fix jsonp

--- C_Sources/Server/igor-assistant-ca-2012/applications/igor/controllers/scheduler.py
+++ C_Sources/Server/igor-assistant-ca-2012/applications/igor/controllers/scheduler.py
@@ -6,61 +6,67 @@
 #########################################################################
 
 from message_packager import *
+from gluon.dal import Rows
+from gluon.tools import Service
+import gluon.contenttype
+
+service = Service()
+
+
+def call():
+	session.forget()
+
+	return service()
 
 # Get scheduler of user at specific term
 # Input:
 # 	integer user_id
 # 	integer term (default = current term)
-@request.restful()
-def get_scheduler():
-    response.view = 'generic.json'
-    def GET(user_id, term = 0):
+@service.jsonp
+@service.json
+def get_scheduler(user_id, term = 0):
 
-    	# phucnh edit 20120726
-        # validate input
-		if (not user_id.isdigit()):
-			return MessagePackager.get_packaged_message (
-				MessageStatus.ERROR, 
-				"user id must be numberic!")
+   	# phucnh edit 20120726
+   	# validate input
+	if (not user_id.isdigit()):
+		return MessagePackager.get_packaged_message (
+			MessageStatus.ERROR, 
+			"user id must be numberic!")
 
-		if (not term.isdigit()):
-			return MessagePackager.get_packaged_message (
-				MessageStatus.ERROR, 
-				"term must be numberic!")
+	if (not term.isdigit()):
+		return MessagePackager.get_packaged_message (
+			MessageStatus.ERROR, 
+			"term must be numberic!")
 
-		if (int (id) < 0):
-			return MessagePackager.get_packaged_message (
-				MessageStatus.ERROR, 
-				"user id can not less than 0!")
+	if (int (id) < 0):
+		return MessagePackager.get_packaged_message (
+			MessageStatus.ERROR, 
+			"user id can not less than 0!")
 
-		if term == 0:
-			term = get_current_term()
+	if term == 0:
+		term = get_current_term()
 
-		# end phucnh edit 20120726
+	# end phucnh edit 20120726
 
-		# Get data from database
-		schedulers = db(
-			db.scheduler.owner == user_id and 
-			db.scheduler.term == term).select()
+	# Get data from database
+	schedulers = db(
+		db.scheduler.owner == user_id and 
+		db.scheduler.term == term).select()
 
-		return
-
-	return locals()
+	return MessagePackager.get_packaged_message(MessageStatus.Ok, schedulers)
 
 # Get scheduler of use at specific term with time
 # Input:
 # 	integer user_id
 # 	integer day_of_week
 # 	integer term (default = current term)
-@request.restful()
-def get_scheduler_by_time():
-    response.view = 'generic.json'
-    def GET(user_id, day_of_week, term):
-        scheduler = db(
-            db.scheduler.owner == user_id and 
-            db.scheduler.day_of_week == day_of_week and
-            db.scheduler.term == term).select()
+@service.jsonp
+@service.json
+def get_scheduler_by_time(user_id, day_of_week, term):
+    
+    scheduler = db(
+    	db.scheduler.owner == user_id and 
+    	db.scheduler.day_of_week == day_of_week and
+    	db.scheduler.term == term).select()
 
-        return MessagePackager.get_packaged_message(MessageStatus.OK, scheduler)
-
-    return locals()
+    return MessagePackager.get_packaged_message(MessageStatus.OK, scheduler)
