5defb5b8d26d198fee0e6acc76c2d8e0afb068b0
hieulq <hieulq89@gmail.com>
2012-08-08 22:51:34 +0700

    Init Android 4.0 project

---
+++ C_Sources/Mobile/2.2/src/com/smilenet/igor/IgorActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Author: SmileNet Team
+ * CyberAgent Contest
+ */
+
+package com.smilenet.igor;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class IgorActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }   
+}
